d31683e0fbcc12dffa55c007f09ace2b6b00813c
mpereannor <mpereannor@gmail.com>
2024-05-26 07:39:12 +0000

    feat: auth config variables

---
+++ src/config/auth.config.ts
@@ -0,0 +1,10 @@
+
+export const config = {
+    authRequired: false,
+    auth0Logout: true,
+    issuerBaseURL: process.env.ISSUER_BASE_URL,
+    baseURL: process.env.BASE_URL,
+    clientID: process.env.CLIENT_ID,
+    secret: process.env.SECRET,
+}
+
