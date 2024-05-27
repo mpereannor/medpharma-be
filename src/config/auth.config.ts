56ef55a67ccd9e4a0a14c5abdb8102d3bdd47dd2
mpereannor <mpereannor@gmail.com>
2024-05-27 05:52:39 +0000

    chore: update auth configuration

--- src/config/auth.config.ts
+++ src/config/auth.config.ts
@@ -1,10 +1,9 @@
-
-export const config = {
-    authRequired: false,
-    auth0Logout: true,
-    issuerBaseURL: process.env.ISSUER_BASE_URL,
-    baseURL: process.env.BASE_URL,
-    clientID: process.env.CLIENT_ID,
-    secret: process.env.SECRET,
+export const authConfig = {
+  authRequired: false,
+  auth0Logout: true,
+  issuerBaseURL: process.env.VITE_AUTH0_ISSUER_BASE_URL || "",
+  audience: process.env.VITE_AUTH0_AUDIENCE || "",
+  baseURL: process.env.BASE_URL,
+  clientID: process.env.AUTH0_CLIENT_ID || "",
+  secret: process.env.AUTH0_CLIENT_SECRET || "",
 }
-
